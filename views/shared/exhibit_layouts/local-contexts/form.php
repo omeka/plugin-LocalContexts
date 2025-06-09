7426f7b1769f15bac6850605fee88c530f8031ab
matthewjmckinley <matthewjamesmckinley@gmail.com>
2025-06-09 16:14:36 -0700

    Build out LC form and display for exhibit builder

---
+++ views/shared/exhibit_layouts/local-contexts/form.php
@@ -0,0 +1,33 @@
+<?php
+$formStem = $block->getFormStem();
+$options = $block->getOptions();
+
+$languageArray = array(
+    'All' => 'All available languages',
+    'English' => 'English',
+    'French' => 'French',
+    'Spanish' => 'Spanish',
+    'Māori' => 'Māori'
+);
+
+$projects = get_option('lc_notices') ? unserialize(get_option('lc_notices')) : [];
+$contentArray = [];
+foreach ($projects as $project) {
+    $contentArray[json_encode($project)] = $project['project_title'];
+}
+?>
+<div class="local-contexts-project">
+    <?php
+    echo $this->formLabel($formStem . '[options][lc-project-id]',
+        __('Select Local Contexts language'));
+    echo $this->formSelect($formStem . '[options][lc-project-language]',
+        @$options['lc-project-language'], array(),
+        $languageArray);
+    echo '</br></br>';
+    echo $this->formLabel($formStem . '[options][lc-project-id]',
+        __('Select Local Contexts project'));
+    echo $this->formSelect($formStem . '[options][lc-project-id]',
+        @$options['lc-project-id'], array(),
+        $contentArray);
+    ?>
+</div>
---
+++ views/shared/exhibit_layouts/local-contexts/layout.php
@@ -0,0 +1,38 @@
+<?php 
+if (!array_key_exists('lc-project-id', $options)) {
+    return;
+}
+
+$project = json_decode($options['lc-project-id'], true);
+$projectArray = array();
+foreach ($project as $key => $content) {
+    if (is_int($key)) {
+        // Only print content in selected language. If 'All', print everything
+        if ((isset($content['language']) && $content['language'] == $options['lc-project-language'])
+        || (!isset($content['language']) && $options['lc-project-language'] == 'English')
+        || $options['lc-project-language'] == 'All') {
+            $projectArray[] = $content;
+        }
+    }
+}
+
+// Don't print project URL if element value array is empty
+if (isset($project['project_url']) && $projectArray) {
+    $projectArray['project_url'] = $project['project_url'];
+    $projectArray['project_title'] = $project['project_title'];
+}
+
+$lcArray = array();
+if ($projectArray) {
+    $lcHtml = LocalContextsPlugin::renderLCNoticeHtml($projectArray);
+    $lcArray['label'] = $lcHtml;
+}
+?>
+
+<?php if (count($lcArray) > 0): ?>
+<div id="local-contexts-content" class="default">
+    <div class='local-contexts-project'>
+        <?php echo $lcArray['label']; ?>
+    </div>
+</div>
+<?php endif; ?>
