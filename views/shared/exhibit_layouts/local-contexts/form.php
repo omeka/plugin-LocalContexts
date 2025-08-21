892b0ed293bb782a5cee1a7bfb686dfa65b90fbc
Kim Nguyen <design@kimisgold.com>
2025-08-21 13:55:39 -0400

    Restructure exhibit block wrappers.

--- views/shared/exhibit_layouts/local-contexts/form.php
+++ views/shared/exhibit_layouts/local-contexts/form.php
@@ -16,18 +16,21 @@ foreach ($projects as $project) {
     $contentArray[json_encode($project)] = $project['project_title'];
 }
 ?>
-<div class="local-contexts-project">
-    <?php
-    echo $this->formLabel($formStem . '[options][lc-project-language]',
-        __('Select Local Contexts language'));
-    echo $this->formSelect($formStem . '[options][lc-project-language]',
-        @$options['lc-project-language'], array(),
-        $languageArray);
-    echo '</br></br>';
-    echo $this->formLabel($formStem . '[options][lc-project-id]',
-        __('Select Local Contexts project'));
-    echo $this->formSelect($formStem . '[options][lc-project-id]',
-        @$options['lc-project-id'], array(),
-        $contentArray);
-    ?>
+<div class="local-contexts-options">
+    <div class="local-contexts-language-select field">
+        <div class="field-meta">
+            <?php echo $this->formLabel($formStem . '[options][lc-project-language]', __('Select Local Contexts language')); ?>
+        </div>
+        <div class="inputs">
+            <?php echo $this->formSelect($formStem . '[options][lc-project-language]', @$options['lc-project-language'], array(), $languageArray); ?>
+        </div>
+    </div>
+    <div class="local-contexts-project-select field">
+        <div class="field-meta">
+            <?php echo $this->formLabel($formStem . '[options][lc-project-id]', __('Select Local Contexts project')); ?>
+        </div>
+        <div class="inputs">
+            <?php echo $this->formSelect($formStem . '[options][lc-project-id]', @$options['lc-project-id'], array(), $contentArray); ?>
+        </div>
+    </div>
 </div>
